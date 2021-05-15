30a3b0bfdd7994c9581ee4d10e7628be8575cb99
leaf-yamamoto <sanaeyamamoto@icloud.com>
2021-05-16 00:24:09 +0900

    ImageViewの完成

--- app/src/main/java/app/yamamoto/leaf/imageview/MainActivity.kt
+++ app/src/main/java/app/yamamoto/leaf/imageview/MainActivity.kt
@@ -3,20 +3,21 @@ package app.yamamoto.leaf.imageview
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.KeyCharacterMap.load
 import android.widget.ImageView
+import coil.api.load
+
+
 import java.lang.System.load
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val url = "https://pics.prcm.jp/bd2976c507fa3/84500725/jpeg/84500725.jpeg"
         val imageView = findViewById<ImageView>(R.id.imageView)
-        imageView.setImageResource(R.drawable.night)
+        imageView.load(url)
 
-        val url ="https://www.google.com/url?sa=i&url=https%3A%2F%2Fgeographical.co.uk%2Fnature%2Fgeophoto%2Fitem%2F3068-photographing-the-night-sky&psig=AOvVaw0NYVzZ5gWCEFCf-ZpNmdQW&ust=1621164873560000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOjy9f_Ly_ACFQAAAAAdAAAAABAD"
-        val imageView = findViewById<ImageView>(R.id.imageView)
-        ImageView.load(url)
     }
 }
